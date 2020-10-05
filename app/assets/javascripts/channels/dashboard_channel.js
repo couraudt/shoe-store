7eedcfed21cea8dc7950fe06997a941ef9284c72
Thibault Couraud <1036233+couraudt@users.noreply.github.com>
2020-10-05 15:36:51 +0200

    change events table classes

--- app/assets/javascripts/channels/dashboard_channel.js
+++ app/assets/javascripts/channels/dashboard_channel.js
@@ -2,7 +2,7 @@ ActionCable.createConsumer().subscriptions.create("DashboardChannel", {
   count: 0,
   received(data) {
     $('#last_events table tbody').prepend($('<tr>')
-      .addClass(this.count % 2 === 0 ? 'odd' : 'even')
+      .addClass(this.count % 2 === 0 ? 'even' : 'odd')
       .append($('<td>')
         .append($('<a>').attr('href', '/stores/'+data.store.id).text(data.store.name))
       )
